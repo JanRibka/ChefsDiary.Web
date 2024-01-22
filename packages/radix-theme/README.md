55d715328b1f97b7ea8712f1c19d5c486f2cbf21
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-01-22 22:41:37 +0100

    Vygenerovaný nový projekt pro Vite

--- packages/radix-theme/README.md
+++
@@ -1,1 +0,0 @@
-# `@chefs-diary/radix-theme`
--- packages/ui/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@chefs-diary/eslint-config/react-internal.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.lint.json",
-  },
-};
