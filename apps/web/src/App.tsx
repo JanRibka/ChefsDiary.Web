312dac9e641fae40c1e12b8cd6478539596b0c98
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-01-23 23:10:02 +0100

    AppTextField

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,12 @@
 import { Theme } from "@radix-ui/themes";
 import { ThemeProvider } from "@repo/radix-theme/ThemeProvider";
+import AppTextField from "@repo/ui/AppTextField";
 
 const App = () => {
   return (
     <ThemeProvider defaultTheme="light" storageKey="ui-theme">
       <Theme accentColor="red" grayColor="slate" className="h-full">
-        Hello world
+        <AppTextField value="dsf" OnChange={() => {}} />
       </Theme>
     </ThemeProvider>
   );
