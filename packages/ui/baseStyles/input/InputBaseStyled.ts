9f880c83e62fcbaeeb7aec09c44d710026d64f9a
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-02-23 14:58:02 +0100

    InputBaseStyled

--- packages/ui/baseStyles/input/InputBaseStyled.ts
+++ packages/ui/baseStyles/input/InputBaseStyled.ts
@@ -1,9 +1,36 @@
-import styled, { css, CSSObject, CSSProperties } from 'styled-components';
+import styled from "styled-components";
 
-// TODO:
-const InputBaseStyled = styled.div<baseprops inputu>`
-  color: red;
-  background-color: ${props => props.variant === 'primary' ? 'darkblue' : 'darkgray'};
-` as CSSProperties;
+export interface InputBaseStyledProps {
+  $size: "small" | "normal";
+}
+
+const Input = styled.input.attrs<{ $size?: string }>((props) => ({
+  // we can define static props
+  type: "text",
+
+  // or we can define dynamic ones
+  $size: props.$size || "1em",
+}))`
+  color: #bf4f74;
+  font-size: 1em;
+  border: 2px solid #bf4f74;
+  border-radius: 3px;
+
+  /* here we use the dynamically computed prop */
+  margin: ${(props) => props.$size};
+  padding: ${(props) => props.$size};
+`;
+
+const InputBaseStyled = styled.div<{ props: InputBaseStyledProps }>(
+  (props) => ({
+    color: "red",
+  })
+);
+//   (
+//   {
+//     width: "100%",
+//     height: props => props.
+//   }
+// );
 
 export default InputBaseStyled;
