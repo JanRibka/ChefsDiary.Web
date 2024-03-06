8375f7ee5c2178bb892ea899a7667ccbb49371f5
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-03-07 00:59:45 +0100

    AppCheckbox

--- apps/web/src/shared/styledComponents/checkbox/AppCheckBox.tsx
+++ apps/web/src/shared/styledComponents/checkbox/AppCheckBox.tsx
@@ -7,21 +7,22 @@ import { CheckLabelBaseProps } from "@repo/shared/interfaces";
 import CheckLabelBase from "../../styledComponentsBase/checkLabelBase/CheckLabelBase";
 
 export interface AppCheckboxProps
-  extends Omit<CheckLabelBaseProps, "id" | "tabIndex"> {}
+  extends Omit<CheckLabelBaseProps, "id" | "htmlFor"> {}
 
 const AppCheckbox = (props: AppCheckboxProps) => {
+  const { label, ...restProps } = props;
   // Constants
   const id = useUniqueId(props.name);
 
   return (
     <div className="flex items-center">
-      <Checkbox.Root id={id} checked>
+      <Checkbox.Root id={id} checked {...restProps}>
         <Checkbox.Indicator>
           <IoCheckboxOutline />
         </Checkbox.Indicator>
       </Checkbox.Root>
 
-      <CheckLabelBase htmlFor={id} />
+      <CheckLabelBase htmlFor={id} label={label} />
     </div>
   );
 };
