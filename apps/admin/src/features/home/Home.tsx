fd0033362ae41c7599c8b2dc43a9beaeeda1cde6
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-05-05 21:39:04 +0200

    Admin - Home

--- apps/admin/src/features/home/Home.tsx
+++ apps/admin/src/features/home/Home.tsx
@@ -1,5 +1,51 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <table
+        style={{
+          backgroundColor: "#f2f2f2",
+          borderRadius: "5px",
+          borderCollapse: "collapse",
+        }}
+      >
+        <thead>
+          <tr>
+            <th>Uživatel</th>
+            <th>Přihlašovací jméno</th>
+            <th>Poslední přihlášení</th>
+            <th>Akce</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr style={{ backgroundColor: "#f2f2f2", borderRadius: "5px" }}>
+            <td>
+              <img src="user_photo.jpg" alt="User Photo" />
+              <span>Jméno uživatele</span>
+              <span>Role</span>
+            </td>
+            <td>Přihlašovací jméno</td>
+            <td>Poslední přihlášení</td>
+            <td>
+              <button>Editovat</button>
+            </td>
+          </tr>
+          <tr style={{ backgroundColor: "#f2f2f2", borderRadius: "5px" }}>
+            <td>
+              <img src="user_photo.jpg" alt="User Photo" />
+              <span>Jméno uživatele</span>
+              <span>Role</span>
+            </td>
+            <td>Přihlašovací jméno</td>
+            <td>Poslední přihlášení</td>
+            <td>
+              <button>Editovat</button>
+            </td>
+          </tr>
+          {/* Add more rows as needed */}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Home;
