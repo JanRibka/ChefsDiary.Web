d2ebe0fa985b78200f6a0226dbf44c641bb15d81
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-05-14 15:36:00 +0200

    Admin - Page base

--- apps/admin/src/shared/styledComponents/pageBase/pageBaseVariants.ts
+++ apps/admin/src/shared/styledComponents/pageBase/pageBaseVariants.ts
@@ -1,7 +1,7 @@
-import { cva } from "class-variance-authority";
+import { cva } from 'class-variance-authority';
 
 export const pageBaseVariants = cva(
-  "bg-dialogBackground h-full ml-0 md:ml-24 flex-1",
+  "bg-dialogBackground h-full ml-0 md:ml-24 flex-1 transition-all duration-700 cubic-bezier(0.9, 0, 0.33, 1)",
   {
     variants: {
       sideBarOpened: {
