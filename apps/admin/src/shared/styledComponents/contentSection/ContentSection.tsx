c5966a80e0a302b2d898fa83c88fa4eca3778b5c
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-05-15 21:47:42 +0200

    Admin - Content section

---
+++ apps/admin/src/shared/styledComponents/contentSection/ContentSection.tsx
@@ -0,0 +1,11 @@
+import ContentSectionProps from "./ContentSectionProps";
+
+const ContentSection = (props: ContentSectionProps) => {
+  return (
+    <div className="bg-white rounded-md w-full m-0 p-0">
+      <div className="p-4">{props.children}</div>
+    </div>
+  );
+};
+
+export default ContentSection;
---
+++ apps/admin/src/shared/styledComponents/contentSection/ContentSectionProps.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+interface ContentSectionProps {
+  children: ReactNode;
+}
+
+export default ContentSectionProps;
