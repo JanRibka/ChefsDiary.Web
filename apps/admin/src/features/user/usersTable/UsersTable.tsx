10311f0a38be0f6814060fd6d0f5711fd068818b
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-06-11 16:21:44 +0200

    Admin - AgGrid

--- apps/admin/src/features/user/usersTable/UsersTable.tsx
+++ apps/admin/src/features/user/usersTable/UsersTable.tsx
@@ -1,3 +1,4 @@
+import { AgGridReact } from "ag-grid-react";
 import { CiEdit } from "react-icons/ci";
 import { MdDeleteOutline } from "react-icons/md";
 
@@ -10,52 +11,54 @@ import useGetAllUsersPaginated from "../../../shared/api/apiHooks/user/useGetAll
 const UsersTable = () => {
   const { paginatedUsers } = useGetAllUsersPaginated();
   // Table definitions
-  const columns: TableColumnDefinition[] = [
-    { label: "Login", field: nameof<User>("login"), type: "string" },
-    { label: "Email", field: nameof<User>("email"), type: "string" },
-    {
-      label: "Zda zakázán",
-      field: nameof<User>("isDisabled"),
-      type: "boolean",
-    },
-    {
-      label: "Registrován dne",
-      field: nameof<User>("createdAt"),
-      type: "date",
-    },
-    {
-      label: "Akce",
-      field: "actions",
-      type: "actions",
-      getActions: ({ id }) => [
-        {
-          label: "Upravit",
-          icon: <CiEdit />,
-          onClick: () => {
-            console.log("Edit user", params.row);
-          },
-        },
-        {
-          label: "Smazat",
-          icon: <MdDeleteOutline />,
-          onClick: () => {
-            console.log("Delete user", params.row);
-          },
-        },
-      ],
-    },
-  ];
+  // const columns: TableColumnDefinition[] = [
+  //   { label: "Login", field: nameof<User>("login"), type: "string" },
+  //   { label: "Email", field: nameof<User>("email"), type: "string" },
+  //   {
+  //     label: "Zda zakázán",
+  //     field: nameof<User>("isDisabled"),
+  //     type: "boolean",
+  //   },
+  //   {
+  //     label: "Registrován dne",
+  //     field: nameof<User>("createdAt"),
+  //     type: "date",
+  //   },
+  //   {
+  //     label: "Akce",
+  //     field: "actions",
+  //     type: "actions",
+  //     getActions: ({ id }) => [
+  //       {
+  //         label: "Upravit",
+  //         icon: <CiEdit />,
+  //         onClick: () => {
+  //           console.log("Edit user", params.row);
+  //         },
+  //       },
+  //       {
+  //         label: "Smazat",
+  //         icon: <MdDeleteOutline />,
+  //         onClick: () => {
+  //           console.log("Delete user", params.row);
+  //         },
+  //       },
+  //     ],
+  //   },
+  // ];
 
-  return (
-    <div>
-      <AppDataGrid
-        name="name"
-        columns={columns}
-        rows={paginatedUsers.data}
-        search
-      />
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <AppDataGrid
+  //       name="name"
+  //       columns={columns}
+  //       rows={paginatedUsers.data}
+  //       search
+  //     />
+  //   </div>
+  // );
+
+  return <AgGridReact rowData={rowData} columnDefs={colDefs} />;
 };
 
 export default UsersTable;
