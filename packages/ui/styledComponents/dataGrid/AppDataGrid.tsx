e8d8fb333d5165027c0737c0935df877756ecca0
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-06-12 21:57:51 +0200

    DataGrid

--- packages/ui/styledComponents/dataGrid/AppDataGrid.tsx
+++ packages/ui/styledComponents/dataGrid/AppDataGrid.tsx
@@ -1,31 +1,24 @@
-import { forwardRef, memo, Ref } from "react";
+import { forwardRef, memo, Ref } from 'react';
 
-import AppDataGridProps from "../props/AppDataGridProps";
-import TableBody from "./dataGridBody/DataGridBody";
-import TableHead from "./dataGridHead/DataGridHead";
-import Search from "./search/Search";
-import { GridValidRowModel } from "./types/gridRows";
+import AppDataGridProps from '../props/AppDataGridProps';
+import TableBody from './dataGridBody/DataGridBody';
+import TableHead from './dataGridHead/DataGridHead';
+import Search from './search/Search';
+import { GridValidRowModel } from './types/gridRows';
 
 const DataGridRaw = forwardRef(function AppDataGrid<
   R extends GridValidRowModel,
 >(props: AppDataGridProps<R>, ref: Ref<HTMLDivElement>) {
+  const { rows, search, searchOnChange, ...restProps } = props;
+
   return (
     <div className="rounded-md border-1 p-4 shadow-md">
       <div className="">
-        <Search search={props.search} />
+        <Search search={search} searchOnChange={searchOnChange} />
         <div className="rounded-md overflow-hidden">
           <table className="w-full text-sm text-left">
-            <TableHead
-              columns={props.columns}
-              name={props.name}
-              getRowId={props.getRowId}
-            />
-            <TableBody
-              rows={props.rows}
-              columns={props.columns}
-              name={props.name}
-              getRowId={props.getRowId}
-            />
+            <TableHead {...restProps} />
+            <TableBody rows={rows} {...restProps} />
           </table>
         </div>
       </div>
